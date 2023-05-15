9f9d622f1f752a06e6a2dd126aff9f8406ea1637
LukeFtz <luke@Luke-2.local>
2023-05-15 17:31:31 -0300

    intial commit

---
+++ RestAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.Interfaces;
+
+namespace RestAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CalculatorController : ControllerBase, ICalculatorInterface
+{
+
+    private readonly ILogger<CalculatorController> _logger;
+
+    public CalculatorController(ILogger<CalculatorController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Aver()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Div()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Mult()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Squa()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IActionResult Sub()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Sum(string firstNumber, string secondNumber)
+    {
+        if(isNumeric(firstNumber) && isNumeric(secondNumber))
+        {
+            var finalValue = convertToDecimal(firstNumber) + convertToDecimal(secondNumber);
+            return Ok(finalValue.ToString()); 
+        }
+
+        return BadRequest("Invalid Input");
+    }
+
+    private bool isNumeric(string strNumber)
+    {
+        double number;
+        bool isNumber = Double.TryParse(
+            strNumber,
+            System.Globalization.NumberStyles.Any,
+            System.Globalization.NumberFormatInfo.InvariantInfo,
+            out number);
+        return isNumber;
+
+    }
+
+    private decimal convertToDecimal(string strNumber)
+    {
+        decimal decimalValue;
+        if (Decimal.TryParse(strNumber, out decimalValue))
+        {
+            return decimalValue;
+        }
+        return 0;
+    }
+}
---
+++ RestAPI/Interfaces/CalculatorInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPI.Interfaces
+{
+	public interface ICalculatorInterface
+	{
+        IActionResult Sum(string firstNumber, string secondNumber);
+        IActionResult Sub();
+        IActionResult Mult();
+        IActionResult Div();
+        IActionResult Aver();
+        IActionResult Squa();
+    }
+}
