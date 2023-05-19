ec396ac74b61308cea026d1f068f245adebb97b6
LukeFtz <luke@Luke-2.local>
2023-05-19 10:05:48 -0300

    Microservice

---
+++ EShop.Api/Model/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EShop.Api.Model.Base
+{
+	public class BaseEntity
+	{
+		[Key]
+		[Column("id")]
+		public long ID { get; set; }
+
+		public BaseEntity()
+		{
+
+		}
+	}
+}
+
---
+++ EShop.Api/Model/Context/MySQLContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Api.Model.Context
+{
+	public class MySQLContext: DbContext
+    {
+		public MySQLContext(){}
+		public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
+		public DbSet<Product> Products { get; set; }
+	}
+}
+
